e3e8607d4996d70739903c36a5e5fa565ec1bdf7
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2015-08-05 01:28:12 -0400

    Make library work across core and photon and move PIN_MAP definition to cpp file

--- firmware/Adafruit_ILI9341.h
+++ firmware/Adafruit_ILI9341.h
@@ -24,8 +24,19 @@ MIT license, all text above must be included in any redistribution
 #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
 #define pgm_read_word(addr) (*(const unsigned short *)(addr))
 
-#define pinLO(_pin)	(PIN_MAP[_pin].gpio_peripheral->BRR = PIN_MAP[_pin].gpio_pin)
-#define pinHI(_pin)	(PIN_MAP[_pin].gpio_peripheral->BSRR = PIN_MAP[_pin].gpio_pin)
+#if defined(PLATFORM_ID)  //Only defined if a Particle device
+  #include "application.h"
+#if (PLATFORM_ID == 0)  // Core
+  #define pinLO(_pin) (PIN_MAP[_pin].gpio_peripheral->BRR = PIN_MAP[_pin].gpio_pin)
+  #define pinHI(_pin) (PIN_MAP[_pin].gpio_peripheral->BSRR = PIN_MAP[_pin].gpio_pin)
+#elif (PLATFORM_ID == 6) // Photon
+  #define pinLO(_pin) (PIN_MAP[_pin].gpio_peripheral->BSRRH = PIN_MAP[_pin].gpio_pin)
+  #define pinHI(_pin) (PIN_MAP[_pin].gpio_peripheral->BSRRL = PIN_MAP[_pin].gpio_pin)
+#else
+  #error "*** PLATFORM_ID not supported by this library. PLATFORM should be Core or Photon ***"
+#endif
+#endif
+
 #define inline inline __attribute__((always_inline))
 
 //typedef unsigned char prog_uchar;
