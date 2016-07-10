0227672e61fbbf78f78449552081d454500d7f98
insano10 <jdommett@gmail.com>
2016-07-10 19:32:53 +0100

    replaced references to BRR and BSRR as mentioned in https://community.particle.io/t/pin-map-issue-with-adafruit-ssd1351-oled-sd-card/16110/5 as it does not verify in the particle build IDE

--- firmware/Adafruit_ILI9341.h
+++ firmware/Adafruit_ILI9341.h
@@ -24,8 +24,8 @@ MIT license, all text above must be included in any redistribution
 #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
 #define pgm_read_word(addr) (*(const unsigned short *)(addr))
 
-#define pinLO(_pin)	(PIN_MAP[_pin].gpio_peripheral->BRR = PIN_MAP[_pin].gpio_pin)
-#define pinHI(_pin)	(PIN_MAP[_pin].gpio_peripheral->BSRR = PIN_MAP[_pin].gpio_pin)
+#define pinLO(_pin)	(pinResetFast(_pin))
+#define pinHI(_pin)	(pinSetFast(_pin))
 #define inline inline __attribute__((always_inline))
 
 //typedef unsigned char prog_uchar;
